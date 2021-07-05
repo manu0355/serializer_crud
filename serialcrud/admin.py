c8c19abf39b45f2ab9d7b22e62e87fba2a8e2637
manu0355 <manojkumar112131@gmail.com>
2021-07-05 20:18:05 +0530

    add class in admin page

--- serialcrud/admin.py
+++ serialcrud/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from serialcrud.models import Company, EmployeeModel
 
 # Register your models here. crud
-admin.site.register(EmployeeModel)
+# admin.site.register(EmployeeModel)
+# admin.site.register(Company)
+class Employeeadmin(admin.ModelAdmin):
+    list_display = ['emp_id','empname','Email','salary','company']
+
+admin.site.register(EmployeeModel, Employeeadmin)
 admin.site.register(Company)
